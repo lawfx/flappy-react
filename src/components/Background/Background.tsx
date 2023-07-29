e899df4b10e23a82f60bb422ad949e50ab4e3449
Nikolaos Ioannou <ioannounikos93@gmail.com>
2023-07-29 16:13:17 +0200

    added a townscape for some horizon visuals

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,9 +1,12 @@
+import Horizon from '../Horizon/Horizon';
 import styles from './Background.module.css';
 
 export default function Background() {
   return (
     <div className={styles.environment}>
-      <div className={styles.sky}></div>
+      <div className={styles.sky}>
+        <Horizon />
+      </div>
       <div className={styles.grass}></div>
       <div className={styles.ground}></div>
     </div>
---
+++ src/components/Horizon/Horizon.tsx
@@ -0,0 +1,9 @@
+import styles from './Horizon.module.css';
+
+export default function Horizon() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.townscape}></div>
+    </div>
+  );
+}
