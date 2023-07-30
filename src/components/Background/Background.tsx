43a06839c026fd70cb0674898ba8c3530571923d
Nikolaos Ioannou <ioannounikos93@gmail.com>
2023-07-30 14:47:11 +0200

    grass now adjusts speed based on parent width

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,16 +2,26 @@ import useGameContext from '@/hooks/useGameContext';
 import Horizon from '../Horizon/Horizon';
 import styles from './Background.module.css';
 import { GameStatus } from '../GameProvider/GameProvider';
+import useElementWidth from '@/hooks/useElementWidth';
+import React from 'react';
+
+const TIME_TO_CROSS_HUNDRED_PIXELS = 1000; //ms
 
 export default function Background() {
   const [state] = useGameContext();
+  const [grassSpeed, setGrassSpeed] = React.useState(0);
+
+  //calculate the speed of the obstacles
+  const wrapperRef = useElementWidth((width: number) => {
+    setGrassSpeed((width / 100) * TIME_TO_CROSS_HUNDRED_PIXELS);
+  }, []);
 
   const gameStatus = state.status;
 
-  const grassStyle = { '--grass-animation-state': gameStatus === GameStatus.Ended ? 'paused' : 'running' } as React.CSSProperties;
+  const grassStyle = { '--grass-speed': grassSpeed, '--grass-animation-state': gameStatus === GameStatus.Ended ? 'paused' : 'running' } as React.CSSProperties;
 
   return (
-    <div className={styles.environment}>
+    <div ref={wrapperRef} className={styles.environment}>
       <div className={styles.sky}>
         <Horizon />
       </div>
